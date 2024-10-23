b0e50f74dd07eda092e4aaaee8a7961601898f7e
Nikki Elisha Plaza <nikkiplaza24@gmail.com>
2024-10-23 19:23:08 +0800

    pseudocode added

---
+++ user_name_and_age_tracker.py
@@ -0,0 +1,7 @@
+# Creating a program that ask user to input name and age.
+# Set definition of valid name and valid age. 
+# Print error message when the input is not valid. 
+# Store all the collected information into array. 
+# After every input, will ask the user if want to input another entry. 
+#   When “Yes”, will ask the user again for input. Doing it until the user respond “No”. 
+#   When the user responded “No”, display the name and age of the oldest person. Use the array in checking who is the oldest.
